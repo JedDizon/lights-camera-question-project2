dd7698cb784ab0f82780fba9e331a7b76c7c8a9a
JedDizon <ku98sovk@students.codeinstitute.net>
2024-11-19 15:18:53 +0000

    added empty array script page

---
+++ assets/js/actadvqs.js
@@ -0,0 +1,77 @@
+let actadvQs = [
+   {
+    question: 'In Indiana Jones and the Last Crusade, who plays Indiana Jones’ father?',
+    correctans: 'Sean Connery',
+    incorrectans: ['Michael Caine', 'Harrison Ford', 'Anthony Hopkins'],
+  },
+   {
+    question: 'What is the name of the island where Jurassic Park is located?',
+    correctans: 'Isla Nublar',
+    incorrectans: ['Isla Muerta', 'Isla Sorna', 'Skull Island'],
+  },
+  {
+   question: 'Which 1980s action movie popularized the catchphrase, "I’ll be back"?',
+   correctans: 'The Terminator',
+   incorrectans: ['Die Hard', 'Predator', 'Total Recall'],
+ },
+ {
+  question: '',
+  correctans: '',
+  incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+{
+ question: '',
+ correctans: '',
+ incorrectans: ['', '', ''],
+},
+];
