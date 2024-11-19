5772c3a9d71020cf98cafeada8cd2dbbac331782
JedDizon <ku98sovk@students.codeinstitute.net>
2024-11-19 11:31:39 +0000

    create functions list skeleton structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,44 @@
-// Function to get all IDs and classes from the document
-function getIdsAndClasses() {
-    const ids = new Set();
-    const classes = new Set();
+function runGame() {
 
-    // Traverse all elements in the document
-    document.querySelectorAll('*').forEach(element => {
-        // Collect ID if it exists
-        if (element.id) {
-            ids.add(element.id);
-        }
-        // Collect classes if they exist
-        element.classList.forEach(cls => classes.add(cls));
-    });
-
-    return {
-        ids: Array.from(ids), // Convert Set to Array
-        classes: Array.from(classes) // Convert Set to Array
-    };
 }
 
-// Usage
-const result = getIdsAndClasses();
-console.log('IDs:', result.ids);
-console.log('Classes:', result.classes);
+function checkAnswer() {
+
+}
+
+function checkCorrectAnswer() {
+
+}
+
+function incrementCorrectScore() {
+
+}
+
+function incrementIncorrectScore() {
+
+}
+
+function displayActAdvQuestions() {
+
+}
+
+function displayHorQuestions() {
+
+}
+
+function displayAniQuestions() {
+
+}
+
+function displayComQuestions() {
+
+}
+
+function displayClaQuestions() {
+
+}
+
+function displayRomQuestions() {
+
+}
+
