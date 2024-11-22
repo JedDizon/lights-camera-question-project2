6a263564369778862c0bd37bcb71957f8ec8ab0b
JedDizon <ku98sovk@students.codeinstitute.net>
2024-11-22 11:37:43 +0000

    Removed displayqs and checkanswer code,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /**
- * glbal variables
+ * global variables
  * dom variables
  * score variables
  * current question
@@ -8,13 +8,13 @@
  * function to run/start game (set game up)
  */
 
-//Global Variables
+// Global variables
 let currentQuestion = 0;
 let score = 0;
-let questionSet = gameType;
-let questionsForQuiz = [];
-let incorrectansForQuiz = [];
-let correctansForQuiz = [];
+let incorrectScore = 0;
+let correctScore = 0;
+let questionSet = []; //array of questions from questionArray.js
+let genreButton = document.getElementById("genre-btn");
 
 
 //Plan
@@ -35,71 +35,27 @@ let correctansForQuiz = [];
 
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
+    let genreButton = document.getElementById("genre-btn");
 
     for (let button of buttons) {
         //as the button is clicked, the code inside will run
         button.addEventListener("click", function(){
-            if (this.getAttribute("type") === "submit") {
-                alert("You clicked an answer!");
-                console.log("You clicked a button");
+            if (this.id === genreButton.id) {
+                console.log("genre button clicked");
+                startGame();
             } else {
-                
+                alert("Please choose a valid genre");
             }
-        })
+        });
     }
-
-    /**Check what genre to load relevant qs */
-    let gameElement = document.getElementById("question");
-    let gameType = gameElement.getAttribute("data-type"); 
-    document.onload = runGame(gameType);
 })
-
-/**
- * Choose genre function
- */
-function chooseGenre() {
-    //Check what genre user choses
-    let genre = prompt("Please choose a genre: Action/Adventure, Horror, Animation, Comedy, Classic, Romance");
-    if (genre === "Action/Adventure") {
-        return "actadvgame";
-    } else if (genre === "Horror") {
-        return "horgame";
-    } else if (genre === "Animation") {
-        return "anigame";
-    } else if (genre === "Comedy") {
-        return "comgame";
-    } else if (genre === "Classic") {
-        return "clagame";
-    } else if (genre === "Romance") {
-        return "romgame";
-    } else {
-        alert("Please choose a valid genre");
-        return chooseGenre();
-    }
-
-
-
-
-        //checks for addition game type
-        if (gameType === 'addition') {
-            displayAdditionQuestion(num1, num2);
-        } else if (gameType === 'multiply') {
-            displayMultiplyQuestion(num1, num2);
-        } else if (gameType === 'subtract') {
-            displaySubtractQuestion(num1, num2);
-        } else if (gameType === 'division') {
-            displayDivideQuestion(num3, num1);
-        } else {
-            alert(`Unknown game type: ${gameType}`);
-            throw `Unknown game type: ${gameType}. Aborting!`;
-        }
-}
 
 
 /**
  * The main game loop
  */
-function runGame(gameType) {
+function startGame(gameType) {
+    console.log("Starting game...");
     if (gameType === "actadvgame") {
         displayActAdvQuestions(
             //input question (set qs, score to zero)
@@ -127,40 +83,6 @@ function checkAnswer(e) {
     //if e.innerhtml === correct answer, increment score
     //if not, display incorrect
     //then calls display question function.
-}
-
-
-//can put into checkAnswer() function
-function checkCorrectAnswer() {
-}
-function incrementCorrectScore() {
-}
-function incrementIncorrectScore() {
-}
-
-//Display questions functions
-function displayActAdvQuestions() {
-
-}
-
-function displayHorQuestions() {
-
-}
-
-function displayAniQuestions() {
-
-}
-
-function displayComQuestions() {
-
-}
-
-function displayClaQuestions() {
-
-}
-
-function displayRomQuestions() {
-
 }
 
 
