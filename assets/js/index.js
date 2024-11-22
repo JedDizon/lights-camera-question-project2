7e9855b25b849a2bccdba96f3a6977b3400d9a80
JedDizon <ku98sovk@students.codeinstitute.net>
2024-11-22 15:36:27 +0000

    creating skeleton for quiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,31 @@
 console.log("index.js loaded");
+let gameArea = document.getElementById("gamearea");
+
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("Game started");
+
+    //Remove intro, rules, genre selector sections
+    const introSectionRemoval = document.getElementById("intro");
+    introSectionRemoval.remove();
+
+    const rulesSectionRemoval = document.getElementById("rules");
+    rulesSectionRemoval.remove();
+    
+    const genreSelectorRemoval = document.getElementById("genre-selector");
+    genreSelectorRemoval.remove();
+
+    // Show the game area
+    gameArea.classList.remove("hidden");
+
+}
+
+function setNextQuestion() {
+  console.log("Next question");
+}
+
+function selectAnswer() {
+  console.log("Answer selected");
+}
